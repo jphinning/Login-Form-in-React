47aa779d5ea0976c7a907eaa6e5c5deb2181f66e
Joao Hinning <jphinning@gmail.com>
2022-02-07 18:40:46 -0300

    Delete All Button Added

--- src/components/ToggleDarkMode.jsx
+++ src/components/ToggleDarkMode.jsx
@@ -1,15 +1,35 @@
-import React from "react"
+import { React, useState } from "react"
 import { Button } from "@chakra-ui/react"
-import { Flex, useColorMode } from "@chakra-ui/react"
-import { IconButton } from '@chakra-ui/react'
+import { Flex, useColorMode, Spacer } from "@chakra-ui/react"
+import { IconButton, CircularProgress } from '@chakra-ui/react'
 import { MoonIcon, SunIcon } from "@chakra-ui/icons"
+import axios from "axios"
 
 const ToggleDarkMode = () => {
-
   const {colorMode, toggleColorMode} = useColorMode()
+  const [isLoadingDelete, setIsLoadingDelete] = useState(false)
 
   return (
-    <Flex justify='flex-end' m='5' gap='6px'>
+    <Flex m='5' gap='6px'>
+      <Button colorScheme={"red"} variant={"solid"}
+        onClick={async () => {
+          setIsLoadingDelete(true)
+          try {
+            await axios.delete(`https://aqueous-coast-43643.herokuapp.com/login/` || `http://localhost:3001/login/`)
+            setIsLoadingDelete(false)
+            window.location.reload()
+          } catch (error) {
+            console.log(error)
+          }
+        }}
+      > 
+        {isLoadingDelete ? (
+          <CircularProgress isIndeterminate size="15px" color="red" />
+        ) : (
+          'Delete All Logs'
+        )}
+      </Button>
+      <Spacer/>
       <IconButton
         variant='outline'
         colorScheme='teal'
